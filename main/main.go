924ecc8493ebd81c5a235eda526c33d04b77d0a6
Blake Bartenbach <blakebartenbach@gmail.com>
2017-04-11 20:52:59 -0500

    Solved challenge 3

--- main/main.go
+++ main/main.go
@@ -5,12 +5,14 @@ import (
     "../challenge-2"
     "fmt"
     "encoding/hex"
+    "bytes"
 )
 
 // proof of completing challenges can be easily uncommented and proven here
 func main() {
     //challenge1()
     //challenge2()
+    //challenge3()
 }
 
 func challenge1() {
@@ -39,4 +41,15 @@ func challenge2() {
     // XOR against each other and encode to hex
     var xored = challenge_2.XORvalues(decoded, decoded2)
     fmt.Println(hex.EncodeToString(xored))
+}
+
+func challenge3() {
+    // get encoded hex value and decode
+    var hex = challenge_1.GetString()
+    var decoded = challenge_1.DecodeHex(hex)
+
+    // would you believe I guessed this immediately?
+    s0 := bytes.Repeat([]byte{ 'X' }, 34)
+    var xored = challenge_2.XORvalues(decoded, s0)
+    fmt.Println(string(xored))
 }
--- readme.md
+++ readme.md
@@ -5,6 +5,4 @@ you can find these challenges at cryptopals.com
 #### Completed
 * Challenge 1
 * Challenge 2
-
-#### In Progress
 * Challenge 3
