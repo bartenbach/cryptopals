2739b39b8f769b527323b367b916994695a15918
Blake Bartenbach <blakebartenbach@gmail.com>
2017-04-26 15:25:30 -0500

    Much better.  Fleshed out the way the file is read and parsed. + Added a script to run tests

--- challenge-2/challenge-2_test.go
+++ challenge-2/challenge-2_test.go
@@ -1,7 +1,6 @@
 package challenge_2
 
 import (
-	"com/blakebartenbach/cryptopals/challenge-2"
 	"encoding/hex"
 	"testing"
 )
@@ -9,7 +8,7 @@ import (
 func TestXORvalues(t *testing.T) { //c
 	val1, err1 := hex.DecodeString("1c0111001f010100061a024b53535009181c")
 	val2, err2 := hex.DecodeString("686974207468652062756c6c277320657965")
-	result, err3 := challenge_2.XORvalues([]byte(val1), []byte(val2))
+	result, err3 := XORvalues([]byte(val1), []byte(val2))
 
 	if err1 != nil || err2 != nil || err3 != nil {
 		t.Fail()
--- challenge-6/challenge-6.go
+++ challenge-6/challenge-6.go
@@ -1,9 +1,12 @@
 package challenge_6
 
 import (
-	"com/blakebartenbach/cryptopals/challenge-4"
-	"encoding/base64"
+//	"encoding/base64"
 	"fmt"
+    "path/filepath"
+    "os"
+    "bufio"
+    "encoding/base64"
 )
 
 var table = [256]uint8{
@@ -27,36 +30,42 @@ var table = [256]uint8{
 
 func Challenge6() {
 	// Read in elements from encrypted file - store them in a slice of EncodedElements (really just strings)
-	var elements []challenge_4.EncodedElement = challenge_4.GetEncodedElements("challenge-6/6.txt")
-	for i := range elements {
-		data, err := base64.StdEncoding.DecodeString(elements[i].Value)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			PrintPrettySlice(data)
-		}
-	}
+    var elements []byte = GetStringFromFile("challenge-6/6.txt")
+    PrintPrettySlice(elements)
 
 	//Guess our keysize.  Will have to find the keysize with the shortest hamming distance.
 	// should I do this manually or programmatically?
 	keysize := 4
 
 	// Using our keysize, calculate hamming distance between two keys
-	for j := 0; j < len(elements); j += keysize {
-		// need to get the first <keysize> worth of bytes
-
+    fmt.Printf("LENGTH OF STRING: %d\n", len(elements))
+    for j:=0; j <= (len(elements)/keysize); j+=(keysize*2) {
+        bytes1 := elements[j : j+keysize]
+        bytes2 := elements[j+keysize : j+(keysize*2)]
+        fmt.Printf("keysize worth of bytes1: ")
+        PrintPrettySlice(bytes1)
+        fmt.Printf("keysize worth of bytes2: ")
+        PrintPrettySlice(bytes2)
+        hammingd, err := HammingDistance(bytes1,bytes2)
+        if err != nil {
+            fmt.Printf("ERROR: %s", err)
+        } else {
+            fmt.Printf("Hamming Distance: %d\n", hammingd)
+        }
+     }
+        // need to get the first <keysize> worth of bytes from the element
 		// wow...this is confusing
-		bytes1 := elements[j : j+keysize]
-		bytes2 := elements[j+keysize : j+(keysize*2)]
-		hammingd, err := HammingDistance(bytes1, bytes2)
-		if err != nil {
-			println("ERROR: ", err)
-		} else {
-			// normalize hamming distance by dividing by the keysize
-			fmt.Printf("Hamming distance: %d\n", hammingd/keysize)
-		}
+//		bytes1 := elements[j : j+keysize]
+//		bytes2 := elements[j+keysize : j+(keysize*2)]
+//		hammingd, err := HammingDistance(bytes1, bytes2)
+//		if err != nil {
+//			println("ERROR: ", err)
+//		} else {
+//			// normalize hamming distance by dividing by the keysize
+//			fmt.Printf("Hamming distance: %d\n", hammingd/keysize)
+//		}
 
-	}
+//	}
 
 }
 
@@ -65,13 +74,10 @@ func HammingDistance(x, y []byte) (int, error) {
 		return 0, fmt.Errorf("String lengths not equal! %d != %d", len(x), len(y))
 	}
 
-	b1 := []byte(x)
-	b2 := []byte(y)
-
 	var hammingd int = 0
 
-	for i := range b1 {
-		hammingd += int(table[(b1[i] ^ b2[i])])
+	for i := range x {
+		hammingd += int(table[(x[i] ^ y[i])])
 	}
 	return hammingd, nil
 }
@@ -80,7 +86,38 @@ func HammingDistance(x, y []byte) (int, error) {
 func PrintPrettySlice(b []byte) {
 	fmt.Printf("[")
 	for x := range b {
-		fmt.Printf("%5d", b[x])
+		fmt.Printf("%4d", b[x])
 	}
-	fmt.Printf("]\n")
+	fmt.Printf(" ]\n")
+}
+
+// reads elements from file and appends them to a list of bytes
+// it also decodes them from base64 because that's convenient
+func GetStringFromFile(fpath string) []byte {
+	path, err := filepath.Abs(fpath)
+	check(err)
+
+	file, err := os.Open(path)
+	check(err)
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var elements []byte
+	for scanner.Scan() {
+        element, err := base64.StdEncoding.DecodeString(scanner.Text())
+        if err != nil {
+            fmt.Printf("ERROR: %s", err)
+        } else {
+		    elements = append(elements, []byte(element)...)
+        }
+	}
+	return elements
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
